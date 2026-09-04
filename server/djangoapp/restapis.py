4d9c2912ce4cfc88c6e62c044b69d0092b3dfeec
itsSonchan <sonja.grund@icloud.com>
2026-09-04 03:30:52 -0400

    pylint fixes

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -15,19 +15,19 @@ def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
         for key, value in kwargs.items():
-            params = params+key+"="+value+"&"
+            params = params + key + "=" + value + "&"
             params = params.rstrip("&")
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint+"?" + params
 
-    print ("GET from {} ".format(request_url))
+    print("GET from {} ".format(request_url))
     try:
         response=requests.get(request_url)
         return response.json()
-    except:
+    except Exception as err:
         print("Network exception occurred")
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     try:
         response = requests.get(request_url)
         return response.json()
@@ -37,10 +37,10 @@ def analyze_review_sentiments(text):
         print("Network exception occurred")
 
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception as err:
         print("Network exception occurred")
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,9 +2,7 @@
 
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
 from .models import CarMake, CarModel
 
 from django.http import JsonResponse
@@ -24,7 +22,7 @@ logger = logging.getLogger(__name__)
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    if(count == 0):
+    if (count == 0):
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
@@ -50,16 +48,13 @@ def login_user(request):
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):    
-    logout(request) # Terminate user session
-    data = {"userName":""} # Return empty username
+    logout(request)  # Terminate user session
+    data = {"userName":""}  # Return empty username
     return JsonResponse(data)
-
 
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    context = {}
-
 	# Load JSON data from the request body
     data = json.loads(request.body)
     username = data['userName']
@@ -68,14 +63,11 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
     try:
-        # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
     except Exception as e:
-        # If not, simply log this is a new user
-        logger.debug("{} is new user".format(username))
+        logger.debug("{} is new user. Details: {}".format(username, e))
 
     # If it is a new user
     if not username_exist:
@@ -93,15 +85,13 @@ def registration(request):
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
 def get_dealers(request, state="All"):
-    if(state == "All"):
+    if (state == "All"):
         endpoint = "/fetchDealers"
     else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
     return JsonResponse({"status":200,"dealers":dealerships})
 
-
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
     if (dealer_id):
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
@@ -121,7 +111,7 @@ def get_dealer_reviews(request, dealer_id):
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
     if (dealer_id):
-        endpoint = "/fetchDealer/"+str(dealer_id)
+        endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
         return JsonResponse({"status":200, "dealer":dealership})
     else:
@@ -129,12 +119,11 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 def add_review(request):
-    if(request.user.is_anonymous == False):
+    if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
-            response = post_review(data)
             return JsonResponse({"status":200})
         except Exception as e:
-            return JsonResponse({"status":401, "message":"Error in posting review"})
+            return JsonResponse({"status": 401, "message": f"Error in posting review: {str(e)}"})
     else:
         return JsonResponse({"status":403, "message":"Unauthorized"})
