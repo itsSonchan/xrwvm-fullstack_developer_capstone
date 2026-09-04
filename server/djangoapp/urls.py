267c414b1ad3451241a93d98cea76e00159328d8
itsSonchan <sonja.grund@icloud.com>
2026-09-04 03:52:34 -0400

    is this it pls

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
     path(route='register', view=views.registration, name='registration'),
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
